4315ce533e65c457a99d1161b9c915d13483355f
Valerij19701971 <kovalevskisvalerijus@gmail.com>
2025-12-11 14:47:17 +0200

    Added JS with addEventListener 2025-12-11-14.47

--- main.js
+++ main.js
@@ -9,4 +9,30 @@ console.log(yearsValue);
 let result = moneyValue + moneyValue * 0.12 * yearsValue;
 console.log(result, typeof (result));
 
-document.querySelector('.balance_input').textContent = result;
+let resultBalance = document.querySelector('.balance_input');
+
+resultBalance.textContent = result
+
+money.addEventListener('input', balanceUpdate);
+
+years.addEventListener('input', function() {
+  balanceUpdate();
+})
+
+
+
+function balanceUpdate() {
+let money = document.querySelector('.initial_sum_input');
+let moneyValue = Number(money.value);
+
+let years = document.querySelector('.years_input');
+let yearsValue = Number(years.value);
+
+let result = moneyValue + moneyValue * 0.12 * yearsValue;
+
+let resultBalance = document.querySelector('.balance_input');
+
+resultBalance.textContent = result
+}
+// balanceUpdate();
+
